103337e3eb96a33a004d3b8e56e070d26d29e333
Omkar5492 <gietomkar@gmail.com>
2025-04-03 15:09:38 +0530

    Updated Project 03-04-2025

---
+++ CODE/FRONTEND/db.sql
@@ -0,0 +1,14 @@
+ 
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS electric_vehicle;
+
+-- Switch to the created database
+USE electric_vehicle;
+
+-- Create the user table
+CREATE TABLE `users` (
+    `name` VARCHAR(225),
+    `email` VARCHAR(225),
+    `password` VARCHAR(225),
+    `Address` VARCHAR(225))
